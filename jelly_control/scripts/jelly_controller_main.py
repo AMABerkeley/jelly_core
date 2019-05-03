f6f967f5a3561150eca546ea77580814e47c7a32
Philipp <phil80301@berkeley.edu>
2019-05-02 19:10:40 -0700

    created launch script, main control script

---
+++ jelly_control/scripts/jelly_controller_main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import time
+import numpy as np
+import rospy
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
+
+class JellyRobot:
+    def __init__(self):
+        # set control rate
+        self.rate = rospy.Rate(200)
+
+        # collect parameters
+        self.joint_names = rospy.get_param("/jelly_hardware/joint_names")
+        self.base_link   = rospy.get_param("/jelly_hardware/base_link")
+        self.fl_link     = rospy.get_param("/jelly_hardware/FL_lower_leg")
+        self.fr_link     = rospy.get_param("/jelly_hardware/FR_lower_leg")
+        self.rl_link     = rospy.get_param("/jelly_hardware/RL_lower_leg")
+        self.rr_link     = rospy.get_param("/jelly_hardware/RR_lower_leg")
+
+        self.gear_ratio  = rospy.get_param("/jelly_hardware/gear_ratio")
+        self.joint_directions = rospy.get_param("/jelly_hardware/joint_directions")
+
+
+        self.leg_odrive_map   = rospy.get_param("/jelly_hardware/leg_odrive_id")
+        self.leg_odrive_axis  = rospy.get_param("/jelly_hardware/leg_odrive_axis")
+        self.motor_publishers = []
+
+        for odrive in self.leg_odrive_map:
+            pi = rospy.Publisher("/jelly_hardware/odrives" + str(odrive)  +"/command", Float64MultiArray, queue_size=1)
+            self.motor_publishers.append(pi)
+            # set up odrive
+            # some command
+            pass
+
+        # set subscribers and publishers
+        #  rospy.Subscriber("/joint_states", JointState, self.update_joints)
+        #  rospy.Subscriber("/motor_states", MotorState, self.update_motors)
+
+        # initalize state
+        self.joint_positions  = [0.0] * len(self.joint_names)
+        self.joint_velocities = [0.0] * len(self.joint_names)
+        self.joint_torques    = [0.0] * len(self.joint_names)
+
+
+    def set_pos(self, cmds):
+        j_cmd = Float64MultiArray()
+        j_cmd.data = cmds
+
+        cmd_idx = 0
+        for pub in self.motor_publishers:
+            self.cmd.publish(j_cmd)
+
+    def update_joints(self, joint_msg):
+        self.joint_positions  = [0.0] * len(self.joint_names)
+        for i, n in enumerate(joint_msg.name):
+            for j, jn in enumerate(self.joint_names):
+                if jn == n:
+                    self.joint_positions[j] = joint_msg.position[i]
+                    continue
+                #  else:
+                    #  rospy.logerr('No Valid Joint Found')
+
+
+if __name__ == '__main__':
+    rospy.init_node('jelly_controller', anonymous=True)
+
+    jelly = JellyRobot()
+    rospy.loginfo('Jelly setup complete')
+
+    # undergo calibration
+    jelly.calibrate()
+
+
+    # initilize to home position
+    jelly.home()
+
+
+    # gui = GUI()
+    # set up gui object that reads from commands from gui
+
+    while not rospy.is_shutdown():
+        # control loop
+
+        # read from gui
+        # mode, cmd = gui.read_command()
+
+        # jelly.command(mode, cmd)
+        jelly.rate.sleep()
+
