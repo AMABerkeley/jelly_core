22634b9c2abd3621b4fc55a155367a964948c029
Joshua Yuan <joshua43210@gmail.com>
2019-05-06 01:39:50 -0700

    accelerate to pwm value

--- jelly_control/scripts/psoc_driver.py
+++ jelly_control/scripts/psoc_driver.py
@@ -5,13 +5,15 @@
 
 import rospy
 import serial
+from serial import SerialException
 import sys
-from std_msgs.msg import String
+from std_msgs.msg import Int32
 
 # https://github.com/BerkeleyExpertSystemTechnologiesLab/2d-spine-control-hardware/blob/master/ros-spine-control/src/spine_controller/src/serial_tx_fromtopic.py
 class SerialTxHubFromTopic:
     def __init__(self, device_name):
         self.serial_tx_startup(device_name)
+        self.pwm_value = 9000
 
     def serial_tx_startup(self, device_name):
         # anonymous=False because we want unique nodes
@@ -22,21 +24,25 @@ class SerialTxHubFromTopic:
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
 
-        sub = rospy.Subscriber("/jelly_hardware/vesc/command", String, self.serial_tx_callback)
+        sub = rospy.Subscriber("/jelly_hardware/vesc/command", Int32, self.serial_tx_callback)
 
         print("Opened serial port and created subscriber.")
         print("Now receiving from topic.")
 
     def serial_tx_callback(self, message):
-        print("message received: " + message.data)
-        try:
-            print("message received: " + message.data)
-            self.res = self.ser.write(message.data + "\r") # ascii \r is 13
-            print(self.res)
-
-            print("message received: " + message.data)
-        except:
-            print("Error writing to serial.")
+        if 8700 < message.data < 9250: # a tested range
+            difference = message.data - self.pwm_value
+            sign = lambda num: (1, -1)[num < 0]
+            increments = abs(difference // 10)
+            for i in range(increments):
+                try:
+                    self.pwm_value += 10 * sign(difference)
+                    print("pwm: " + str(self.pwm_value))
+                    self.ser.write(str(self.pwm_value) + "\r")
+                except SerialException as e:
+                    print("Serial failed: " + str(e))
+        else:
+            print(str(message.data) + " is not between 8700 and 9250")
 
 if __name__ == "__main__":
     device_name = "/dev/serial/by-id/usb-Cypress_Semiconductor_Cypress_KitProg_16170ADE002E4400-if02"
