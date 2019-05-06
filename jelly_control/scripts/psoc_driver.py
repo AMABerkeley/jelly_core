1e690355347861c507bcb0b6f8279a5dc8e9f556
Philipp <phil80301@berkeley.edu>
2019-05-06 00:41:17 -0700

    updated psoc driver

--- jelly_control/scripts/psoc_driver.py
+++ jelly_control/scripts/psoc_driver.py
@@ -1,4 +1,4 @@
-#!/user/bin/python
+#!/usr/bin/env python
 
 # serial_tx_hub subscribes to a String rostopic and writes to the serial port.
 # It is used to control the hub motor via pwm through the psoc 5lp.
@@ -10,33 +10,36 @@ from std_msgs.msg import String
 
 # https://github.com/BerkeleyExpertSystemTechnologiesLab/2d-spine-control-hardware/blob/master/ros-spine-control/src/spine_controller/src/serial_tx_fromtopic.py
 class SerialTxHubFromTopic:
-    def __init__(self, device_name, topic_name):
-        self.serial, self.sub = self.serial_tx_startup(device_name)
+    def __init__(self, device_name):
+        self.serial_tx_startup(device_name)
 
     def serial_tx_startup(self, device_name):
         # anonymous=False because we want unique nodes
         rospy.init_node("serial_tx_hub_from_topic", anonymous=False)
 
         baud_rate = 115200; # must be consistent with the psoc UART component
-        ser = serial.Serial(device_name, baud_rate)
-        ser.reset_input_buffer()
-        ser.reset_output_buffer()
+        self.ser = serial.Serial(device_name, baud_rate)
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
 
         sub = rospy.Subscriber("/jelly_hardware/vesc/command", String, self.serial_tx_callback)
 
         print("Opened serial port and created subscriber.")
         print("Now receiving from topic.")
-        return ser, sub
 
     def serial_tx_callback(self, message):
-        print("message received: " + message)
+        print("message received: " + message.data)
         try:
-            ser.write(message + "\r") # ascii \r is 13
+            print("message received: " + message.data)
+            self.res = self.ser.write(message.data + "\r") # ascii \r is 13
+            print(self.res)
+
+            print("message received: " + message.data)
         except:
             print("Error writing to serial.")
 
 if __name__ == "__main__":
-    device_name = "TODO fill in"
+    device_name = "/dev/serial/by-id/usb-Cypress_Semiconductor_Cypress_KitProg_16170ADE002E4400-if02"
     s_tx = SerialTxHubFromTopic(device_name)
 
     try:
